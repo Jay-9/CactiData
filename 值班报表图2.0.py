0b8d43e886f4a51b9dce2fac22feb4f7b93e6a0b
Jay-9 <j60968@163.com>
2020-11-03 16:25:20 +0800

    the_end!

--- 值班报表图2.0.py
+++ 值班报表图2.0.py
@@ -5,6 +5,23 @@ import pythoncom
 import os
 import wget
 
+'''import xlwings as xw
+def excel_pic2_linux(the_every_duty_file):
+    app = xw.App(visible=True, add_book=False)
+    wb = app.books.open(the_every_duty_file)
+    sht = wb.sheets['Sheet1']
+    range_val = sht.range((1, 1), (8, 5))
+    range_val.api.CopyPicture()
+    sht.api.Paste()
+    pic = sht.pictures[0]
+    pic.api.Copy()
+    img = ImageGrab.grabclipboard()
+    img.save('/jay/ok.png')
+    pic.delete()
+    wb.close()
+    app.quit()
+    # os.remove('/jay/ok.xlsx')'''
+
 print('\n程序正在执行请等待……')
 wget.download('ftp://shiyan:123@10.2.205.6/jay/ok.xlsx', out=os.getcwd()+'\\ok.xlsx')
 pythoncom.CoInitialize()
@@ -24,20 +41,3 @@ excel.Quit()
 pythoncom.CoUninitialize()
 os.remove(os.getcwd()+'\\ok.xlsx')
 x = input('\n\n程序执行完毕,按回车键后退出！')
-
-'''import xlwings as xw
-def excel_pic2_linux(the_every_duty_file):
-    app = xw.App(visible=True, add_book=False)
-    wb = app.books.open(the_every_duty_file)
-    sht = wb.sheets['Sheet1']
-    range_val = sht.range((1, 1), (8, 5))
-    range_val.api.CopyPicture()
-    sht.api.Paste()
-    pic = sht.pictures[0]
-    pic.api.Copy()
-    img = ImageGrab.grabclipboard()
-    img.save('/jay/ok.png')
-    pic.delete()
-    wb.close()
-    app.quit()
-    # os.remove('/jay/ok.xlsx')'''
